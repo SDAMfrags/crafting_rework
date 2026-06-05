e3ce24e12c69db317d1d1145eb673e5b237a875c
SDAMfrags <shadow86693@gmail.com>
2026-06-05 08:47:04 +0530

    Initial commit

---
+++ src/main/java/net/sdam/crafting_rework/CraftingReworkClient.java
@@ -0,0 +1,10 @@
+package net.sdam.crafting_rework;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftingReworkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sdam/crafting_rework/CraftingReworkDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sdam.crafting_rework;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CraftingReworkDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
